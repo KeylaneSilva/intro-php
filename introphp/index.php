b65bc8b7050ba879243d3646b5bf8fd244168394
Keylane Silva <keylanessilva07@gmail.com>
2021-03-09 11:34:52 -0300

    init

---
+++ introphp/index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Formulario de inscrição</title>
+    <meta name ="autor" content="">
+    <meta name ="descrição" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+</head>
+<body>
+    <p>FORMULÁRIO PARA A INSCRIÇÃO DE COMPETIDORES</p>
+
+    <form action="script.php" method="POST">
+    <p>Seu nome: <input type="text" name="nome"></p>
+    <p>Sua idade: <input type="text" name="idade"></p>
+    <p><input type="submit" value="Enviar dados"></p>
+    </form>
+
+
+</body>
+</html>
