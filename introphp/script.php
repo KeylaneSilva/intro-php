06b535dcdf63748f05aa8a11ca14a582d7d9317c
Keylane Silva <keylanessilva07@gmail.com>
2021-03-09 11:35:27 -0300

    categorias on

---
+++ introphp/script.php
@@ -0,0 +1,56 @@
+<?php
+
+$categorias = [];
+$categorias[] = 'infantil';
+$categorias[] = 'adolescente';
+$categorias[] = 'adulto';
+$categorias[] = 'idoso';
+//print_r($categorias);
+
+$nome = $_POST['nome'];
+$idade = $_POST['idade'];
+
+var_dump($nome);
+var_dump($idade);
+return 0;
+
+/*
+if($idade >= 6 && $idade <= 12)
+{
+    for($i = 0; $i <= count($categorias); $i++)
+    {
+        if($categorias[$i] == 'infantil')
+        {
+            echo "O nadador " .$nome. " compete na categoria " .$categorias[$i];
+        }
+    }
+}else if($idade >= 13 && $idade <= 18)
+{
+    for($i = 0; $i <= count($categorias); $i++)
+    {
+        if($categorias[$i] == 'adolescente')
+        {
+            echo "O nadador " .$nome. " compete na categoria " .$categorias[$i];
+        }
+    }
+}else if($idade >= 19 && $idade <= 50)
+{
+    for($i = 0; $i <= count($categorias); $i++)
+    {
+        if($categorias[$i] == 'adulto')
+        {
+            echo "O nadador " .$nome. " compete na categoria " .$categorias[$i];
+        }
+    }
+}else if($idade >= 51 && $idade <= 100)
+{
+    for($i = 0; $i <= count($categorias); $i++)
+    {
+        if($categorias[$i] == 'idoso')
+        {
+            echo "O nadador " .$nome. " compete na categoria " .$categorias[$i];
+        }
+    }
+}
+*/
+?>
