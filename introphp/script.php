472fcb8c876e4a7db72b16d9856fbf79a7cf73cb
Keylane Silva <keylanessilva07@gmail.com>
2021-03-09 22:14:03 -0300

    validacao de dados

--- introphp/script.php
+++ introphp/script.php
@@ -10,11 +10,39 @@ $categorias[] = 'idoso';
 $nome = $_POST['nome'];
 $idade = $_POST['idade'];
 
+/*
 var_dump($nome);
 var_dump($idade);
 return 0;
+*/
 
-/*
+if(empty($nome))
+{
+    echo "Campo nome vazio";
+    return;
+}
+
+if(empty($idade))
+{
+    echo "Campo idade vazio";
+    return;
+}
+
+if(strlen($nome) < 3)
+{
+    echo "Nome muito pequeno";
+    return;
+}
+
+if(strlen($nome) > 40)
+{
+    echo "Nome muito extenso";
+    return;
+}
+
+
+
+
 if($idade >= 6 && $idade <= 12)
 {
     for($i = 0; $i <= count($categorias); $i++)
@@ -52,5 +80,4 @@ if($idade >= 6 && $idade <= 12)
         }
     }
 }
-*/
 ?>
