d3fda9363b82d41dd08fdad2a79aee9be0cb7894
Keylane Silva <46472697+KeylaneSilva@users.noreply.github.com>
2021-03-15 00:38:10 -0300

    alteracões para a validacão

--- introphp/script.php
+++ introphp/script.php
@@ -6,49 +6,16 @@ $categorias[] = 'infantil';
 $categorias[] = 'adolescente';
 $categorias[] = 'adulto';
 $categorias[] = 'idoso';
-//print_r($categorias);
+//print_r($categorias); 
 
 $nome = $_POST['nome'];
 $idade = $_POST['idade'];
 
 /*
-var_dump($nome);
+var_dump($nome); 
 var_dump($idade);
 return 0;
 */
-
-if(empty($nome))
-{
-    $_SESSION['mensagem-de-erro'] = 'O nome não pode ser vazio, preencha novamente';
-    header('location: index.php');
-    return;
-}
-else if(strlen($nome) < 3)
-{
-    $_SESSION['mensagem-de-erro'] = 'Nome muito curto, preencha novamente';
-    header('location: index.php');
-    return;
-}
-else if(strlen($nome) > 40)
-{
-    $_SESSION['mensagem-de-erro'] = 'Nome muito longo, preencha novamente';
-    header('location: index.php');
-    return;
-}
-else if(empty($idade))
-{
-    $_SESSION['mensagem-de-erro'] = 'A idade não pode ser vazia, preencha novamente';
-    header('location: index.php');
-    return;
-}
-
-else if(!is_numeric($idade))
-{
-    $_SESSION['mensagem-de-erro'] = 'A idade tem que ser um número, preencha novamente';
-    header('location: index.php');
-    return;
-}
-
 
 if($idade >= 6 && $idade <= 12)
 {
