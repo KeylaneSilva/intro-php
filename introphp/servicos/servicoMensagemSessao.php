8757b9fa80a4405397ce1b1ab454a0ef6021eeb3
Keylane <keylanessilva07@gmail.com>
2021-03-16 17:49:21 -0300

    Funcções de parametrização das validações

--- introphp/servicos/servicoMensagemSessao.php
+++ introphp/servicos/servicoMensagemSessao.php
@@ -10,6 +10,30 @@ function setarMensagemErro(string $mensagem) : void
 function obterMensagemErro() : ?string
 {
     if(isset($_SESSION['mensagem-de-erro']))
-        return $_SESSION['mensagem-de-erro']
+        return $_SESSION['mensagem-de-erro'];
     return null;
+}
+
+function setarMensagemSucesso(string $mensagem) : void
+{
+    $_SESSION['mensagem-de-sucesso'] = $mensagem;
+}
+
+function ObterMensagemSucesso() : ?string
+{
+    if(isset($_SESSION['mensagem-de-sucesso']))
+        return $_SESSION['mensagem-de-sucesso'];
+    return null;
+}
+
+function removerMensagemErro() : void
+{
+    if(isset($_SESSION['mensagem-de-erro']))
+        unset($_SESSION['mensagem-de-erro']);
+}
+
+function removerMensageSucesso() : void
+{
+    if(isset($_SESSION['mensagem-de-sucesso']))
+        unset($_SESSION['mensagem-de-sucesso']);
 }
