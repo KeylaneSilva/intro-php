4167cbe5c4d80a9c5afe8a77df111ee11bd8bbc7
Keylane <keylanessilva07@gmail.com>
2021-03-22 23:42:59 -0300

    index

--- introphp/index.php
+++ introphp/index.php
@@ -1,7 +1,6 @@
 <?php
     include "servicos/servicoMensagemSessao.php";
 ?>
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -11,11 +10,9 @@
     <meta name ="autor" content="">
     <meta name ="descrição" content="">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-
 </head>
 <body>
     <p>FORMULÁRIO PARA A INSCRIÇÃO DE COMPETIDORES</p>
-
     <form action="script.php" method="POST">
     <?php
   
@@ -34,8 +31,7 @@
         {
             echo $mensagemDeSucesso;
         }
-    }
-      
+    } 
     ?>
     <p>Seu nome: <input type="text" name="nome"></p>
     <p>Sua idade: <input type="text" name="idade"></p>
